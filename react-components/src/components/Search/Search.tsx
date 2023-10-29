f0ffc9425fe86b6e6a5b4d2c093c8264b35ebf71
Natallia Ivanyuk <natashaivanyuk@gmail.com>
2023-10-30 01:47:20 +0300

    refactor: delete extra code and files

--- react-components/src/components/Search/Search.tsx
+++ react-components/src/components/Search/Search.tsx
@@ -1,22 +1,11 @@
 import { Component } from "react";
 import './search.scss';
 
-type SearchProp = {
-  query: string;
-};
-
-type SearchState = {
-  isQuery: boolean;
-};
-
-export class Search extends Component<SearchProp, SearchState> {
-  state = {
-    isQuery: false,
-  }
+export class Search extends Component {
   render() {
     return (
       <div className="search-wrapper">
-        <input className="search-input"/>
+        <input className="search-input" placeholder="Search artist"/>
         <button className="search-button"/>
       </div>
     )
--- react-components/src/main.tsx
+++ react-components/src/main.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import './index.scss';
 import Search from './components/Search';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Search query=''/>
+    <Search/>
   </React.StrictMode>
 );
