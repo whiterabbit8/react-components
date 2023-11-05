2a35fa290ec324b049d7d723f15bd430b6b4d255
Natallia Ivanyuk <natashaivanyuk@gmail.com>
2023-11-05 21:29:11 +0300

    feat: add pagination
    feat: add pagination
    
    feat: add pagination

--- react-rss/src/components/Search/Search.tsx
+++ react-rss/src/components/Search/Search.tsx
@@ -1,84 +1,57 @@
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import { Character } from '../../utilities/types';
 import SearchResults from '../SearchResults/SearchResults';
+import SearchInput from '../SearchInput/SearchInput';
+import Pagination from '../Pagination/Pagination';
+import baseUrl from '../../utilities/constants';
 
 import './search.scss';
 
-export default class Search extends Component {
-  state = {
-    query: '',
-    baseUrl: 'https://rickandmortyapi.com/api/character/',
-    page: 1,
-    characters: [],
-    isLoading: true,
-    hasFound: false,
-    hasError: false,
-  };
+export default function Search(): JSX.Element {
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const [characters, setCharacters] = useState<Character[]>([]);
+  const [isLoading, setLoading] = useState(true);
+  const [hasFound, setFound] = useState(false);
+  const [hasError, setError] = useState(false);
 
-  componentDidMount(): void {
-    const value = localStorage.getItem('query')
-      ? localStorage.getItem('query')
-      : '';
-    this.setState({ query: value });
-    this.makeSearch(value).then(() => this.setState({ isLoading: false }));
-  }
-
-  setStorage = () => {
-    localStorage.setItem('query', this.state.query);
-  };
-
-  makeSearch = async (name: string | null) => {
-    this.setState({ isLoading: true, hasFound: false });
-    const searchUrl = name ? `name=${name.trim().replace(' ', '+')}` : '';
-    const response = await fetch(
-      `${this.state.baseUrl}?page=${this.state.page}&${searchUrl}`,
-      {
+  useEffect(() => {
+    const getPage = async () => {
+      setLoading(true);
+      const name = localStorage.getItem('query');
+      const searchUrl = name ? `name=${name.trim().replace(' ', '+')}` : '';
+      const response = await fetch(`${baseUrl}?page=${page}&${searchUrl}`, {
         method: 'GET',
-      }
-    );
-    if (response.status === 200) {
-      this.setState({ hasFound: true });
+      });
       const data = await response.json();
-      const results: Character[] = data.results;
-      this.setState({ characters: results });
-    }
-  };
+      setCharacters(data.results);
+    };
+    getPage().then(() => setLoading(false));
+  }, [page]);
 
-  render() {
-    if (this.state.hasError) throw new Error('App is crashed!');
-    return (
-      <div className="container">
-        <div className="search-wrapper">
-          <input
-            className="search-input"
-            placeholder="enter rick and morty character name"
-            value={this.state.query}
-            onChange={(e) => this.setState({ query: e.target.value })}
-          />
-          <button
-            className="search-button"
-            onClick={() => {
-              this.setStorage();
-              this.makeSearch(this.state.query).then(() =>
-                this.setState({ isLoading: false })
-              );
-            }}
-          />
-        </div>
-        <button
-          className="error-button"
-          onClick={() => this.setState({ hasError: true })}
-        >
-          Throw Error
-        </button>
-        {this.state.isLoading && <h2>Loading...</h2>}
-        {!this.state.isLoading && !this.state.hasFound && (
-          <h2>Character has not found</h2>
-        )}
-        {!this.state.isLoading && this.state.hasFound && (
-          <SearchResults characters={this.state.characters} />
-        )}
-      </div>
-    );
-  }
+  useEffect(() => {
+    setPage(1);
+  }, [hasFound]);
+
+  if (hasError) throw new Error('App is crashed!');
+
+  return (
+    <div className="container">
+      <SearchInput
+        setFound={setFound}
+        setLoading={setLoading}
+        setTotalPages={setTotalPages}
+        setCharacters={setCharacters}
+      />
+      <button className="error-button" onClick={() => setError(true)}>
+        Throw Error
+      </button>
+      {isLoading && <h2>Loading...</h2>}
+      {!isLoading && !hasFound && <h2>Character has not found</h2>}
+      {!isLoading && hasFound && <SearchResults characters={characters} />}
+      {!isLoading && hasFound && (
+        <Pagination active={page} total={totalPages} setPage={setPage} />
+      )}
+    </div>
+  );
 }
---
+++ react-rss/src/utilities/constants.ts
@@ -0,0 +1,3 @@
+const baseUrl = 'https://rickandmortyapi.com/api/character/';
+
+export default baseUrl;
