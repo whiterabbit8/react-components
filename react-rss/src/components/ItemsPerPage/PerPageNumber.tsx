f93a37f1c9107d7fa50b53b5b1a912a562321a15
Natallia Ivanyuk <natashaivanyuk@gmail.com>
2023-11-22 19:23:50 +0300

    fix: loader ui and inputs error

--- react-rss/src/components/ItemsPerPage/PerPageNumber.tsx
+++ react-rss/src/components/ItemsPerPage/PerPageNumber.tsx
@@ -11,12 +11,12 @@ type PerPageNumberParams = {
 export default function PerPageNumber({
   displayedValue,
 }: PerPageNumberParams): JSX.Element {
-  const perPage = useSelector((state: RootState) => state.perPage.value)
+  const perPage = useSelector((state: RootState) => state.perPage.value);
   const dispatch = useDispatch();
 
   const handleClick = (value: number) => {
-    dispatch(setPerPage(value))
-  }
+    dispatch(setPerPage(value));
+  };
 
   return (
     <>
@@ -25,6 +25,7 @@ export default function PerPageNumber({
         id={`${displayedValue}`}
         className="per-page__number-input"
         checked={perPage === displayedValue}
+        onChange={() => {}}
       />
       <label
         htmlFor="5"
