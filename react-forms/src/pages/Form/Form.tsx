35ed7e875f137b05a2a10cdfea0433b460056755
Natallia Ivanyuk <natashaivanyuk@gmail.com>
2023-12-03 19:20:56 +0300

    feat: add form

--- react-forms/src/pages/Form/Form.tsx
+++ react-forms/src/pages/Form/Form.tsx
@@ -1,3 +1,39 @@
 export default function Form(): JSX.Element {
-  return <div>Form</div>;
+  return (
+  <form className="form">
+    <div className="form-element">
+      <label htmlFor="name">Name:</label>
+      <input type="text" name="name" className="input" />
+    </div>
+    <div className="form-element">
+      <label htmlFor="age">Age:</label>
+      <input type="text" name="age" className="input" />
+    </div>
+    <div className="form-element">
+      <label htmlFor="email">Email:</label>
+      <input type="text" name="email" className="input" />
+    </div>
+    <div className="form-element">
+      <label htmlFor="password">Password:</label>
+      <input type="text" name="password" className="input" />
+    </div>
+    <div className="form-element">
+      <label htmlFor="password-confirm">Confirm password:</label>
+      <input type="text" name="password-confirm" className="input" />
+    </div>
+    <div className="form-element">
+      <label htmlFor="password-confirm">Gender:</label>
+      <select name="password-confirm" className="input">
+        <option>Choose...</option>
+        <option>Male</option>
+        <option>Female</option>
+      </select>
+    </div>
+    <div className="form-element checkbox">
+      <input type="checkbox" name="acceptance" />
+      <label htmlFor="acceptance">Agree to terms and conditions</label>
+    </div>
+    <button type="submit" className="submit-btn">Submit</button>
+  </form>
+  );
 }
--- react-forms/src/store/formSlice.tsx
+++ react-forms/src/store/formSlice.tsx
@@ -1,6 +1,6 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
-export interface FormState {
+export interface FormData {
   name: string;
   age: number;
   email: string;
@@ -9,47 +9,50 @@ export interface FormState {
   acceptance: boolean;
 }
 
+export interface FormState {
+  submitted: FormData[];
+}
+
 const initialState: FormState = {
-  name: '',
-  age: 21,
-  email: '',
-  password: '',
-  gender: '',
-  acceptance: false,
+  // name: '',
+  // age: 21,
+  // email: '',
+  // password: '',
+  // gender: '',
+  // acceptance: false,
+  submitted: [],
 };
 
 export const formSlice = createSlice({
   name: 'formData',
   initialState,
   reducers: {
-    setName: (state, action: PayloadAction<string>) => {
-      state.name = action.payload;
-    },
-    setAge: (state, action: PayloadAction<number>) => {
-      state.age = action.payload;
-    },
-    setEmail: (state, action: PayloadAction<string>) => {
-      state.email = action.payload;
-    },
-    setPassword: (state, action: PayloadAction<string>) => {
-      state.email = action.payload;
-    },
-    setGender: (state, action: PayloadAction<string>) => {
-      state.gender = action.payload;
-    },
-    setAcceptance: (state, action: PayloadAction<boolean>) => {
-      state.acceptance = action.payload;
-    },
+    // setName: (state, action: PayloadAction<string>) => {
+    //   state.name = action.payload;
+    // },
+    // setAge: (state, action: PayloadAction<number>) => {
+    //   state.age = action.payload;
+    // },
+    // setEmail: (state, action: PayloadAction<string>) => {
+    //   state.email = action.payload;
+    // },
+    // setPassword: (state, action: PayloadAction<string>) => {
+    //   state.email = action.payload;
+    // },
+    // setGender: (state, action: PayloadAction<string>) => {
+    //   state.gender = action.payload;
+    // },
+    // setAcceptance: (state, action: PayloadAction<boolean>) => {
+    //   state.acceptance = action.payload;
+    // },
+    setSubmitted: (state, action: PayloadAction<FormData>) => {
+      state.submitted.push(action.payload);
+    }
   },
 });
 
 export const {
-  setName,
-  setAge,
-  setEmail,
-  setPassword,
-  setGender,
-  setAcceptance,
+  setSubmitted
 } = formSlice.actions;
 
 export default formSlice.reducer;
